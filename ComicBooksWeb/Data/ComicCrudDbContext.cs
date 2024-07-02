54c3a268e2495d906f2271f107c4d00b68a4084f
Aaron Campbell <aaronc@petaf.org>
2024-07-02 19:50:09 -0400

    Successful DB migration to local SQL server

--- ComicBooksWeb/Data/ComicCrudDbContext.cs
+++ ComicBooksWeb/Data/ComicCrudDbContext.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ComicBooksWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ComicBooksWeb.Data
 {
     public class ComicCrudDbContext : DbContext
     {
-        public ComicCrudDbContext(DbContextOptions<DbContext> options) : base(options)
+        public ComicCrudDbContext(DbContextOptions<ComicCrudDbContext> options) : base(options)
         {
-            
+               
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
