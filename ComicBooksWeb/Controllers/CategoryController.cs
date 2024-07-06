f8dae565af73dfd3546d044d286573eab0ab76f6
Aaron Campbell <aaronc@petaf.org>
2024-07-06 17:03:16 -0400

    Added Toastr for success and error messages

--- ComicBooksWeb/Controllers/CategoryController.cs
+++ ComicBooksWeb/Controllers/CategoryController.cs
@@ -28,13 +28,61 @@ namespace ComicBooksWeb.Controllers
             {
                 ModelState.AddModelError("name", "Category Name cannot be the exact same as the Display Order");
             }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "The value of \"Test\" is invalid.");
+            }
             if (ModelState.IsValid)
             {
                 _context.Categories.Add(obj); // Adds Category object to the Category table 
                 _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index"); // Redirect to Category controller
             }
             return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDb == null) { return NotFound(); }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(obj); // Adds Category object to the Category table 
+                _context.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index"); // Redirect to Category controller
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryFromDb == null) { return NotFound(); }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (obj == null) { return NotFound(); }
+            _context.Categories.Remove(obj);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index"); // Redirect to Category controller
         }
     }
 }
